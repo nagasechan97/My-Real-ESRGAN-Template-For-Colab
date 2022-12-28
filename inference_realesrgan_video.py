b4a72ef9b727ee5f0cdc16266ab955166b397bbf
nagasechan97 <67129230+nagasechan97@users.noreply.github.com>
2022-12-28 08:59:15 +0800

    Update inference_realesrgan_video.py

--- inference_realesrgan_video.py
+++ inference_realesrgan_video.py
@@ -151,7 +151,7 @@ class Writer:
                                  pix_fmt='yuv420p',
                                  vcodec='libx265',
                                  crf='12',
-                                 preset='veryslow',
+                                 preset='slow',
                                  loglevel='error',
                                  acodec='copy').overwrite_output().run_async(
                                      pipe_stdin=True, pipe_stdout=True, cmd=args.ffmpeg_bin))
@@ -159,7 +159,7 @@ class Writer:
             self.stream_writer = (
                 ffmpeg.input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f'{out_width}x{out_height}',
                              framerate=fps).output(
-                                 video_save_path, pix_fmt='yuv420p', vcodec='libx265', crf='12', preset='veryslow',
+                                 video_save_path, pix_fmt='yuv420p', vcodec='libx265', crf='12', preset='slow',
                                  loglevel='error').overwrite_output().run_async(
                                      pipe_stdin=True, pipe_stdout=True, cmd=args.ffmpeg_bin))
 
